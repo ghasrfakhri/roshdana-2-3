6220984530d5e0b905d32f0949f6821cfa454020
Iman Ghasrfakhri <ghasrfakhri@gmail.com>
2021-05-29 17:52:17 +0430

    Add password hash

--- login.php
+++ login.php
@@ -3,9 +3,9 @@ require 'include/init.php';
 $msg = '';
 if (isPostMethod()) {
     $email = $_POST['email'];
-    $password = $_POST['password'];
+    $hash = sha1($_POST['password']);
 
-    $query = "SELECT * FROM user WHERE email='$email' and password='$password'";
+    $query = "SELECT * FROM user WHERE email='$email' and password='$hash'";
     $result = $db->query($query);
 
     if ($result->num_rows > 0) {
@@ -30,6 +30,7 @@ if (isPostMethod()) {
     <title>Document</title>
 </head>
 <body>
+
 <?= $msg ?>
 <form method="post">
     <label>
@@ -40,5 +41,7 @@ if (isPostMethod()) {
     </label><br>
     <input type="submit" value="Login">
 </form>
+
+<a href="register.php">Registration</a>
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,61 @@
+<?php
+require 'include/init.php';
+
+$mgs = "";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $firstname = $_REQUEST['firstname'];
+    $lastname = $_REQUEST['lastname'];
+    $email = $_REQUEST['email'];
+    $password = $_REQUEST['password'];
+    $passwordRepeat = $_REQUEST['password_repeat'];
+
+    if ($password != $passwordRepeat) {
+        $mgs = "Password and its repeat is not equal";
+    } else {
+        $mgs = "Registration Complete";
+        $hash = sha1($password);
+        $query = "INSERT INTO user SET firstname='$firstname', lastname='$lastname', email='$email', password='$hash' ";
+        $result = $db->query($query);
+    }
+
+
+}
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo $mgs;
+
+
+?>
+<form method="post" enctype="multipart/form-data">
+    <label>
+        Firstname: <input name="firstname">
+    </label><br>
+    <label>
+        Lastname: <input name="lastname">
+    </label><br>
+    <label>
+        Email: <input name="email">
+    </label><br>
+    <label>
+        Password: <input name="password" type="password">
+    </label><br>
+    <label>
+        repeat Password: <input name="password_repeat" type="password">
+    </label><br>
+
+    <input type="submit" value="Add User">
+</form>
+</body>
+</html>
